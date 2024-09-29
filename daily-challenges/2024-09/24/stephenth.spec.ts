3591002cd1e9eeb09b9b9e1f537938e3b2bd60f3
Stephen Truong Trung Hieu <trunghieu.truong.bit@gmail.com>
2024-09-29 12:04:53 +0700

    update

--- daily-challenges/2024-09/24/stephenth.spec.ts
+++ daily-challenges/2024-09/24/stephenth.spec.ts
@@ -3,6 +3,7 @@ import { test, expect, type Page } from '@playwright/test';
 test.describe('Mocking an API call', () => {
 
   test('mocks a fruit and does not call api', async ({ page }) => {
+    console.log("hey")
     // Mock the api call before navigating
     await page.route('*/**/api/v1/fruits', async (route) => {
       const json = [{ name: 'Strawberry', id: 21 }];
